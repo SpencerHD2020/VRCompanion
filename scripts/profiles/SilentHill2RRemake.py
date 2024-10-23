9c0dd0c700a762c75d4917d9f70fc952b62cee25
Spencer Nowlin <spencernowlin@icloud.com>
2024-10-22 19:55:35 -0500

    SH2R Profile

---
+++ scripts/profiles/SilentHill2RRemake.py
@@ -0,0 +1,15 @@
+# Weapon 1: Right Grip near Right Hip Holster (Handgun)
+gestureTracker.holsterWeaponRight.enabled = True
+gestureTracker.holsterWeaponRight.gripAction = KeyPress(Key.D1)
+
+# Weapon 2: Right Grip near Right Shoulder Holster (Shotgun)
+gestureTracker.shoulderWeaponRight.enabled = True
+gestureTracker.shoulderWeaponRight.gripAction = KeyPress(Key.D2)
+
+# Weapon 3: Right Grip near Left Shoulder Holster (Hunting Rifle)
+gestureTracker.shoulderWeaponLeft.enabled = True
+gestureTracker.shoulderWeaponLeft.gripAction = KeyPress(Key.D3)
+
+# Weapon 4: Right Grip near Left Hip Holster (Map)
+gestureTracker.holsterWeaponLeft.enabled = True
+gestureTracker.holsterWeaponLeft.gripAction = KeyPress(Key.M)
